252bdbc5966b62f8bbaa95d0d8258f8fa874d052
Kiran Khayamali <kirankhayamali@gmail.com>
2026-03-27 10:02:00 +0545

    Learning about dependencies

--- fastapi/starting.py
+++ fastapi/starting.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Form, HTTPException, Response
+from fastapi import Cookie, Depends, FastAPI, Form, HTTPException, Response
 from fastapi.responses import JSONResponse, RedirectResponse
 from pydantic import BaseModel, HttpUrl
 
@@ -72,16 +72,57 @@ item_db = [
     {"item_id": 11, "name": "K", "price": 550.2},
 ]
 
+user_db = [
+    {"username": "Ashura", "fullname": "Kiran Khayamali"},
+    {"username": "JohnDoe", "fullname": "John Doe"},
+]
+
+#Class as Dependency
+class dependency_skip():
+    def __init__(self, q: str| None = None, skip: int = 0, limit: int = 10):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+async def dependency_model(item: Item, user: User | None = None):
+    return {"item": item, "user": user if user else None}
+
+#Sub dependencies
+def dependency_query(q: str | None = None):
+    return q
+
+def dependency_query_or_cookie(
+    q: Annotated[str | None, Depends(dependency_query)], 
+    last_query: Annotated[str | None, Cookie()] = None
+):
+    print("Cookie value: ", last_query)
+    if q:
+        return q
+    return ("Query not found, cookie value: " + last_query) if last_query else "No query or cookie found" #Both postman and swagger ui doesn't support cookie, so we can test it using browser dev panels.
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World!"}
 
+#Dependency Injection
+# common_annotation = Annotated[dependency_skip, Depends(dependency_skip)] 
+common_annotation = Annotated[dependency_skip, Depends()] #FastAPI provides shortcut where dependency are specially a class, it will call to create an instance of the class
+common_model_annotation = Annotated[dict, Depends(dependency_model)]
+
 #Query parameters
-@app.get("/items/")
-def read_items(skip: int = 0, limit: int = 10):
-    return item_db[skip : skip + limit]
+@app.get("/items/", tags=["items"], deprecated=True)
+def read_items(commons: common_annotation):
+    return item_db[commons.skip: commons.skip + commons.limit]
+
+@app.get("/users/", tags=["users"])
+def read_users(commons: common_annotation):
+    return user_db[commons.skip: commons.skip + commons.limit]
+
+@app.get("/query/")
+def read_query(query: Annotated[str, Depends(dependency_query_or_cookie)]):
+    return {"q_or_cookie": query}
+
 
 # @app.get("/items/{item_id}")
 # async def read_item(item_id: int, q: str | None = None):
@@ -90,7 +131,7 @@ def read_items(skip: int = 0, limit: int = 10):
 #         raise HTTPException(status_code=404, detail="Item not found")
 #     if q:   
 #         return {"item_id": item_id, "q": q}
-#     return {"item": item_db[item_id - 1]}
+#     return {"item": item_db[item_id - 1x]}
 
 @app.exception_handler(StarletteHTTPException)
 async def custom_http_exception_handler(request, exc):
@@ -102,7 +143,7 @@ async def custom_validation_exception_handler(request, exc):
     print(f"Validation error at {request.url}: {exc.errors()}")
     return await request_validation_exception_handler(request, exc)
 
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}", tags=["items"])
 async def read_item(item_id: int):
     if item_id == 4:
         raise HTTPException(status_code=444, detail= "The 4th item is not available")
@@ -117,7 +158,7 @@ async def read_item(item_id: int):
 #Multiple path and query parameters
 @app.get("/users/{user_id}/items/{item_id}")
 async def read_user_item(
-    user_id: int, item_id: str, q: str | None = None, short: bool = False
+    user_id: int, item_id: int, q: str | None = None, short: bool = False
 ):
     item = {"item_id": item_id, "owner_id": user_id}    
     if q:
@@ -128,13 +169,22 @@ async def read_user_item(
         )
     return item
 
-@app.post("/items/")
-def create_item(item: Item, user: User | None = None):
-    item_dict = item.model_dump()
-    if item.tax:
-        price_with_tax = item.price + item.tax
+@app.post("/items/", summary="Create an item", response_description="The created item", tags=["items"])
+def create_item(common: common_model_annotation):
+    '''
+        Create an item with all the informations:
+        - **name**: each item must have a name
+        - **description**: a long description of the item 
+        - **price**: required price of the item
+        - **tax**: if the item doesn't have tax, you can skip this field
+        - **tags**: a set of unique tag strings for the item
+        - **image**: an optional image of the item, which should be a valid URL and a name for the image
+    '''
+    item_dict = common["item"].model_dump()
+    if common["item"].tax:
+        price_with_tax = common["item"].price + common["item"].tax
         item_dict.update({"price_with_tax": price_with_tax})
-    return (item_dict, user if user else None)
+    return (item_dict, common["user"] if common["user"] else None)
 
 # #Request body + path parameters
 # @app.put("/items/{item_id}")
@@ -145,9 +195,9 @@ def create_item(item: Item, user: User | None = None):
 #     return result
 
 #Multiple body parameters
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item, user: User):
-    return {"item_id": item_id, "item": item, "user": user}
+@app.put("/items/{item_id}", tags=["items"])
+def update_item(item_id: int, common: common_model_annotation):
+    return {"item_id": item_id, "item": common["item"], "user": common["user"]}
 
 #Bodies of pure lists
 @app.post("/images/multiple/")
@@ -156,7 +206,7 @@ async def create_multiple_images(images: list[Image]): #when using def only Fast
 
 #Return Response Directly
 @app.get("/portal")
-async def get_portal(teleport: bool = False) -> Response:
+async def get_portal(teleport: bool = False) -> Response:   
     if teleport:
         return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     return JSONResponse(content={"message": "Here's your interdimensional portal."})
@@ -165,3 +215,4 @@ async def get_portal(teleport: bool = False) -> Response:
 @app.post("/login/")
 async def login(form: Annotated[FormData, Form()]):
     return form
+
