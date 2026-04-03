560c5df5585729ed54eec27e18952d00f5d040ac
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-03 13:14:22 +0545

    Adding user model and relationship between users and todo_lists

--- ToDo/models.py
+++ ToDo/models.py
@@ -1,7 +1,18 @@
-from sqlalchemy import Column, Integer, String
-from database import Base 
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base 
 from pydantic import BaseModel
+from typing import List
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String)
+    password = Column(String)
+
+    notes = relationship("Note", back_populates="owner")
+    
 class Note(Base):
     __tablename__ = "todo_lists"
 
@@ -9,10 +20,27 @@ class Note(Base):
     title = Column(String, unique=True, index=True)
     memo = Column(String)
 
+    user_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="notes")
+
 class NoteSchema(BaseModel):
     id: int | None = None
     title: str
     memo: str 
 
-    class Config:
-        orm_mode = True
+    model_config = {
+        "from_attributes": True  
+    }
+
+class UserCreate(BaseModel):
+    username: str 
+    password: str 
+
+class UserSchema(UserCreate):
+    id: int | None = None
+    username: str 
+    notes: List[NoteSchema] = []
+
+    model_config = {
+        "from_attributes": True
+    }
