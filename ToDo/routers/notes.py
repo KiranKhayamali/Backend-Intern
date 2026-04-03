6fd3b5cc84120e1ede9a0f791464856326a84fac
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-03 13:16:17 +0545

    Scaling Todo file using Router

---
+++ ToDo/routers/notes.py
@@ -0,0 +1,70 @@
+from typing import Annotated
+from fastapi import  Depends, Query, HTTPException, status, APIRouter
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from ..models import Note, NoteSchema
+from ..deps import get_db
+from ..auth import get_current_user
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_db)]
+
+router=APIRouter()
+
+@router.get("/")
+async def read_notes(db:SessionDep, offset:int = 0, limit: Annotated[int, Query(le=10)] = 10):
+    select_sql = select(Note).offset(offset).limit(limit)
+    result = await db.execute(select_sql)
+    notes = result.scalars().all()
+    if not notes:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No Notes on Todo list")
+    return notes
+
+@router.get("/{note_id}")
+async def read_note(note_id:int, db:SessionDep):
+    note = await db.get(Note, note_id)
+    if not note:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note Not Found")
+    return note
+
+@router.post("/")
+async def create_note(note:NoteSchema, db: SessionDep, current_user = Depends(get_current_user)):
+    db_note = Note(**note.model_dump())
+    db.add(db_note)
+    await db.commit()
+    await db.refresh(db_note)
+    return db_note
+
+@router.put("/{note_id}")
+async def update_note(note_id: int, note:NoteSchema, db:SessionDep):
+    note_db = await db.get(Note, note_id)
+    if not note_db:
+        db_note = Note(**note.model_dump())
+        db.add(db_note)
+        await db.commit()
+        await db.refresh(db_note)
+        return {"message": f"{db_note.title} has been created to the todo list."} 
+
+    existing = await db.execute(
+        select(Note).where(
+            Note.title == note.title,
+            Note.id != note_id
+        )
+    )
+    if existing.scalar():
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Title doesn't exists on {note_id} in the database")
+
+    if note_db.title== note.title:
+        note_db.memo = note.memo
+    await db.commit()
+    await db.refresh(note_db)
+    return note_db
+
+@router.delete("/{note_id}")
+async def delete_note(note_id:int, db:SessionDep):
+    note = await db.get(Note, note_id)
+    if not note:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note Not Found")
+    await db.delete(note) 
+    await db.commit()
+    return {"message": f"{note.title} has been successfully removed from the todo list."}
