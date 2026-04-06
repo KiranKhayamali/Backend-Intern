f76367d83862bd0389a517c2564337200f1b2897
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-06 10:06:20 +0545

    Removing the authentication function from the file

--- ToDo/routers/users.py
+++ ToDo/routers/users.py
@@ -2,13 +2,30 @@ from typing import Annotated, List
 from fastapi import Depends, Query, HTTPException, status, APIRouter
 from sqlalchemy import select
 from sqlalchemy.orm import selectinload
-from sqlalchemy.ext.asyncio import AsyncSession
-from ..models import User, UserSchema
-from ..deps import get_db
+from fastapi.security import OAuth2PasswordRequestForm
+from ..auth import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, get_current_active_user, authenticate_user, get_password_hash
+from datetime import timedelta
 
-SessionDep = Annotated[AsyncSession, Depends(get_db)]
+from ..models import User, UserSchema
+from ..deps import SessionDep
+
+
 
 router = APIRouter()
+
+
+@router.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: SessionDep):
+    user = await authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect username or password")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": str(user.id)}, expires_delta=access_token_expires)
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@router.get("/me")
+async def read_users_me(current_user: Annotated[User, Depends(get_current_active_user)]):
+    return current_user
 
 @router.get("/", response_model=List[UserSchema])
 async def read_users(db:SessionDep, offset:int = 0, limit: Annotated[int, Query(le=5)] = 5):
@@ -19,10 +36,12 @@ async def read_users(db:SessionDep, offset:int = 0, limit: Annotated[int, Query(
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User Not Found")
     return users
 
+
 @router.post("/")
 async def create_user(user:UserSchema, db:SessionDep):
-    print(user.model_dump())
-    db_user = User(**user.model_dump())
+    user_data = user.model_dump()
+    user_data["password"] = get_password_hash(user.password)
+    db_user = User(**user_data)
     db.add(db_user)
     await db.commit()
     await db.refresh(db_user)
