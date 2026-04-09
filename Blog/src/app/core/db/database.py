dbffd6d5e7590d51589ad3e80e4914876005ec69
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-09 13:07:22 +0545

    database connection with ORM SQLAlchemy

---
+++ Blog/src/app/core/db/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import async_sessionmaker, DeclarativeBase, MappedAsDataclass
+from ..config import settings
+from typing import AsyncGenerator
+
+
+class Base(DeclarativeBase, MappedAsDataclass):
+    pass
+
+
+DATABASE_URI = settings.POSTGRES_URI
+DATABASE_PREFIX = settings.POSTGRES_ASYNC_PREFIX
+DATABASE_URL = f"{DATABASE_PREFIX}{DATABASE_URI}"
+
+
+async_engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+
+local_session = async_sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def async_get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with local_session() as db:
+        yield db
