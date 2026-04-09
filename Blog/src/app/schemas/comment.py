2450920e281f16972af24c6a494c69144f5572df
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-09 13:01:46 +0545

    Design schema for comment that create,read,update,delete comments

---
+++ Blog/src/app/schemas/comment.py
@@ -0,0 +1,49 @@
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Annotated
+from ..core.schemas import TimestampSchema, PersistentDeletion, UUIDSchema
+from datetime import datetime
+
+
+class CommentBase(BaseModel):
+    content:Annotated[str, Field(min_length=1, max_length=500)]
+
+
+class Comment(CommentBase, TimestampSchema, PersistentDeletion, UUIDSchema):
+    author_id: int 
+    post_id: int
+    likes: int = 0
+
+
+class CommentRead(BaseModel):
+    id: int 
+    content: str 
+    author_id: int 
+    post_id: int
+    likes: int = 0
+    created_at: datetime
+
+
+class CommentCreate(CommentBase):
+    model_config = ConfigDict(extra="forbid")
+
+
+class CommentCreateInternal(CommentBase):
+    author_id: int 
+    post_id: int
+
+
+class CommentUpdate(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+    content: Annotated[str, Field(min_length=1, max_length=500, default=None)]
+
+
+class CommentUpdateInternal(CommentUpdate):
+    updated_at: datetime
+
+
+class CommentDelete(BaseModel):
+    model_config = ConfigDict(extra="forbid")
+
+    is_deleted: bool = True
+    deleted_at: datetime
