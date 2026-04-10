c738a0bfc2dd66ec2a40ac56d52f660e2d257ab4
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-10 14:33:57 +0545

    Connecting database with the Postgres Server@

--- Blog/src/app/core/db/database.py
+++ Blog/src/app/core/db/database.py
@@ -1,11 +1,6 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import async_sessionmaker, DeclarativeBase, MappedAsDataclass
+from sqlalchemy.ext.asyncio import create_async_engine
 from ..config import settings
-from typing import AsyncGenerator
-
-
-class Base(DeclarativeBase, MappedAsDataclass):
-    pass
+from ...models.base import Base
 
 
 DATABASE_URI = settings.POSTGRES_URI
@@ -15,9 +10,11 @@ DATABASE_URL = f"{DATABASE_PREFIX}{DATABASE_URI}"
 
 async_engine = create_async_engine(DATABASE_URL, echo=False, future=True)
 
-local_session = async_sessionmaker(bind=async_engine, class_=AsyncSession, expire_on_commit=False)
+async def init_db() -> None:
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 
-async def async_get_db() -> AsyncGenerator[AsyncSession, None]:
-    async with local_session() as db:
-        yield db
+async def drop_db() -> None:
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
