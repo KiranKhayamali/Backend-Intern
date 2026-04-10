89dc19fbe84a649d125b52fff13261e2abfddb7e
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-10 14:39:13 +0545

    Designed model for posts

--- Blog/src/app/models/posts.py
+++ Blog/src/app/models/posts.py
@@ -1,9 +1,7 @@
 from sqlalchemy import String, ForeignKey, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import UTC, datetime
-from ..core.db.database import Base
-from .comments import Comment
-from .users import User
+from .base import Base
 
 
 
@@ -12,14 +10,11 @@ class Post(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, init=False)
     author_id: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True)
+    author: Mapped[str] = relationship("User", back_populates="posts")
     title: Mapped[str] = mapped_column(String(100), nullable=False)
     content: Mapped[str] = mapped_column(String(1000), nullable=False)
 
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=lambda: datetime.now(UTC))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default_factory=lambda: datetime.now(UTC))
     updated_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), default=None)
     deleted_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), default=None)
     is_deleted: Mapped[bool] = mapped_column(default=False, index=True)
-
-    comments: Mapped[list["Comment"]] = relationship("Comment", back_populates="post")
-    likes: Mapped[int] = mapped_column(default=0)
-    liked_by_users: Mapped[list["User"]] = relationship("User", secondary="post_likes", back_populates="liked_posts")
