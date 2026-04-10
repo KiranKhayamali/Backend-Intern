d320718e4b4990d064ce18a3dc4b6b9616ef9f58
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-10 14:40:22 +0545

    Designed schema for user basics

--- Blog/src/app/schemas/user.py
+++ Blog/src/app/schemas/user.py
@@ -14,22 +14,19 @@ class User(UUIDSchema, UserBase, TimestampSchema, PersistentDeletion):
     profile_picture: Annotated[str, Field(default="https://unsplash.com/photos/a-gorilla-sitting-on-the-ground-QGdmkyLK7jo")]
     hashed_password: str 
     is_admin: bool = False
-    tier_id: int | None = None
     posts: list | None = None  # This will be populated with the user's posts when needed
-    likes: list | None = None  # This will be populated with the user's liked posts when needed
-    comments: list | None = None  # This will be populated with the user's comments when needed
+
 
 class UserRead(BaseModel):
     id: int | None = None 
     username: Annotated[str, Field(min_length=3, max_length=100)]
     email: Annotated[EmailStr, Field(examples=["user@example.com", "user.name@example.com"])] 
-    profile_picture: str
-    tier_id: int | None = None
+    profile_picture: Annotated[str, Field(examples=["https://unsplash.com/photos/a-gorilla-sitting-on-the-ground-QGdmkyLK7jo"], default=None)]
 
 class UserCreate(UserBase):
     model_config = ConfigDict(extra="forbid") # raise error if extra fields are provided
 
-    password: Annotated[str, Field(min_length=6, max_length=20, pattern=r"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$", examples=["Password1", "MySecurePass123"])] 
+    password: Annotated[str, Field(min_length=6, max_length=20, examples=["Password1", "MySecurePass123"])] 
 
 class UserCreateInternal(UserBase):
     hashed_password: str
@@ -44,8 +41,6 @@ class UserUpdate(BaseModel):
 class UserUpdateInternal(UserUpdate):
     updated_at: datetime
 
-class UserUpdateTier(BaseModel):
-    tier_id: int 
 
 class UserDelete(BaseModel):
     model_config = ConfigDict(extra="forbid")
