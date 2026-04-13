064a6ddc714c171df757b1f00ef284ee506d9854
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-13 13:32:04 +0545

    Model for blacklisting used token after logged out

--- Blog/src/app/core/db/token_blacklist.py
+++ Blog/src/app/core/db/token_blacklist.py
@@ -3,7 +3,8 @@ from datetime import datetime
 from sqlalchemy import DateTime, String
 from sqlalchemy.orm import Mapped, mapped_column
 
-from .database import Base
+from ...models.base import Base
+
 
 
 class TokenBlacklist(Base):
