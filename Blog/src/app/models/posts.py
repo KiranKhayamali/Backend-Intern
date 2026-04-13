cc85520cf1338e421ac030994be71c106ed23d5d
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-13 10:16:35 +0545

    Adds author_name to the posts table

--- Blog/src/app/models/posts.py
+++ Blog/src/app/models/posts.py
@@ -1,18 +1,21 @@
+from typing import TYPE_CHECKING
 from sqlalchemy import String, ForeignKey, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import UTC, datetime
 from .base import Base
 
-
+if TYPE_CHECKING:
+    from .users import User
 
 class Post(Base):
     __tablename__ = "posts"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, init=False)
     author_id: Mapped[int] = mapped_column(ForeignKey("users.id"), index=True)
-    author: Mapped[str] = relationship("User", back_populates="posts")
+    author: Mapped["User"] = relationship("User", back_populates="posts", init=False)
     title: Mapped[str] = mapped_column(String(100), nullable=False)
     content: Mapped[str] = mapped_column(String(1000), nullable=False)
+    author_name: Mapped[str] = mapped_column(String(20), nullable=False)
 
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default_factory=lambda: datetime.now(UTC))
     updated_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True), default=None)
