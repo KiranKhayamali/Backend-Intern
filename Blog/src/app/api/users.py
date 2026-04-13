1fa56c5079762bfae70d8e1a997c8b1a560fd3c4
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-13 11:33:36 +0545

    Updating user while using both email and username

--- Blog/src/app/api/users.py
+++ Blog/src/app/api/users.py
@@ -1,12 +1,13 @@
 from typing import Annotated, Any
 from fastapi import APIRouter, Depends, Request
 from fastcrud import PaginatedListResponse, compute_offset, paginated_response
+from datetime import datetime, UTC
 
 from ..core.dependencies import SessionDep
 from ..core.db.session import async_get_db
 from ..core.security import get_password_hash
 from ..core.exceptions.http_exceptions import DuplicateValueException, ForbiddenException, NotFoundException
-from ..schemas.user import UserRead, UserCreate, UserCreateInternal, UserUpdate
+from ..schemas.user import UserRead, UserCreate, UserCreateInternal, UserUpdate, UserUpdateInternal
 from ..repositories.user_repository import crud_users
 from ..core.dependencies import get_current_active_user
 
@@ -65,3 +66,37 @@ async def create_user(request: Request, user: UserCreate, db: Annotated[SessionD
         raise NotFoundException("Failed to create the user!!!")
     
     return created_user
+
+@router.put("/{username_or_email}", response_model=UserRead)
+async def update_user(username_or_email:str, user_update: UserUpdate, current_user: Annotated[UserRead, Depends(get_current_active_user)], db: Annotated[SessionDep, Depends(async_get_db)]) -> dict[str, Any]:
+    if "@" in username_or_email:
+        db_user = await crud_users.get(db=db, email=username_or_email, is_deleted=False, schema_to_select=UserRead)
+    else:
+        db_user = await crud_users.get(db=db, username=username_or_email, is_deleted=False, schema_to_select=UserRead)
+    
+    if not db_user:
+        raise NotFoundException("User Not Found!!!")
+
+    if current_user["id"] != db_user["id"]:
+        raise ForbiddenException()
+    
+    user_update_dict = user_update.model_dump(exclude_unset=True)
+    if "password" in user_update_dict:
+        user_update_dict["hashed_password"] = get_password_hash(password=user_update_dict["password"])
+        del user_update_dict["password"]
+    
+    user_update_dict["updated_at"] = datetime.now(UTC)
+    user_update_internal_dict = UserUpdateInternal(**user_update_dict).model_dump(exclude_unset=True)
+
+    updated_user = await crud_users.update(
+        db=db,
+        object=user_update_internal_dict,
+        schema_to_select=UserRead,
+        return_as_model=True,
+        id=db_user["id"],
+    )
+
+    if updated_user is None:
+        raise NotFoundException("Failed to update user!!!")
+
+    return updated_user
