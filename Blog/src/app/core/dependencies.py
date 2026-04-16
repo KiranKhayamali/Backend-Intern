0d58dcb5b6a9c6ae405a8a5c7aa266f5b60d9af1
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-16 10:14:50 +0545

    Adding optional_user that logs errors

--- Blog/src/app/core/dependencies.py
+++ Blog/src/app/core/dependencies.py
@@ -1,21 +1,22 @@
 from __future__ import annotations
 from typing import Annotated, TYPE_CHECKING
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status, Request
 
 
 from .db.session import async_get_db
 from ..repositories.user_repository import crud_users
 from ..schemas.user import UserRead
-from .security import oauth2_scheme
+from .security import TokenType, oauth2_scheme, verify_token
+from .logger import logging
 
 
 SessionDep = Annotated[AsyncSession, Depends(async_get_db)]
 
+logger = logging.getLogger(__name__)
+
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: SessionDep):
-    from .security import TokenType, oauth2_scheme, verify_token
-
     token_data = await verify_token(token, TokenType.ACCESS, db)
     if not token_data:
         return HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User not authenticated!!!")
@@ -30,8 +31,34 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Se
     
     return user
 
+
 async def get_current_active_user(current_user: Annotated[UserRead, Depends(get_current_user)]):
     if getattr(current_user, "disabled", False):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user")
     return current_user
 
+
+async def get_optional_user(request: Request, db:SessionDep) -> dict | None:
+    token = request.headers.get("Authorization")
+    if not token:
+        return None 
+
+    try:
+        token_type, _, token_value = token.partition(" ")
+        if token_type.lower() != "bearer" or not token_value:
+            return None 
+
+        token_data = await verify_token(token_value, TokenType.ACCESS, db)
+        if token_data is None:
+            return None 
+
+        return await get_current_user(token_value, db=db)
+    
+    except HTTPException as http_exec:
+        if http_exec.status_code != 401:
+            logger.error(f"Unexpected HTTPExection in get_optional_user: {http_exec.detail}")
+        return None 
+
+    except Exception as exec:
+        logger.error(f"Unexpected error in get_optional_user: {exec}")
+        return None
