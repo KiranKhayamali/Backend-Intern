7b109126e7a1a6f436ed3a59d955825a4f755aaf
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-17 14:11:58 +0545

    Initializing fastcrud for comment

---
+++ Blog/src/app/repositories/comment_repository.py
@@ -0,0 +1,7 @@
+from fastcrud import FastCRUD
+from ..models.comments import Comment 
+from ..schemas.comment import CommentCreateInternal, CommentRead, CommentUpdate, CommentUpdateInternal, CommentDelete
+
+
+CRUDComments = FastCRUD[Comment, CommentCreateInternal, CommentRead, CommentUpdate, CommentUpdateInternal, CommentDelete]
+crud_comments = CRUDComments(Comment)
