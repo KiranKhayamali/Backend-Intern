9bb623bf867b6104d9aec7bbadec96ec5520ef55
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-21 12:02:07 +0545

    Revocating tokens using redis

--- Blog/src/app/api/logout.py
+++ Blog/src/app/api/logout.py
@@ -1,28 +1,38 @@
 from typing import Optional, Annotated, Any
 from fastapi import APIRouter, Cookie, Depends, Response
-from jose import JWTError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..core.exceptions.http_exceptions import UnauthorizedException
 from ..core.security import blacklist_tokens, oauth2_scheme
 from ..core.db.session import async_get_db
+from ..core.db.redis_connect import redis_client
 
 
-router = APIRouter(tags=["login"], prefix="/users")
+router = APIRouter(tags=["login"], prefix="/users/auth")
 
 
 @router.post("/logout")
-async def logout(response: Response, access_token: str = Depends(oauth2_scheme), refresh_token: Optional[str] = Cookie(None, alias="refresh_token"), db: AsyncSession = Depends(async_get_db)) -> dict[str, Any]:
+async def logout(
+    response: Response, 
+    access_token: str = Depends(oauth2_scheme), 
+    refresh_token: Optional[str] = Cookie(None, alias="refresh_token"), 
+    db: AsyncSession = Depends(async_get_db)
+) -> dict[str, Any]:
     try:
         if not refresh_token:
             raise UnauthorizedException("Refresh Token Not Found!!!")
         
-        await blacklist_tokens(access_token=access_token, refresh_token=refresh_token, db=db)
+        if access_token.startswith("Bearer "):
+            access_token = access_token.split(" ")[1]
+        
+        await blacklist_tokens(access_token=access_token, db=db)
+
+        redis_client.delete(f"auth:refresh:{refresh_token}")
         response.delete_cookie(key="refresh_token")
 
         return {"message": "Logged Out Successfully!!!"}
     
-    except JWTError:
-        raise UnauthorizedException("Invalid Token!!!")
+    except Exception as exec:
+        raise UnauthorizedException(f"Invalid Token!!! Details: {exec}")
 
 
