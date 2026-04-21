7d8b8ca154b6f89d8005c6daf7ba290fea3b3255
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-21 13:37:39 +0545

    Formatting files to make code style consistent and cleaner

--- Blog/src/app/repositories/user_repository.py
+++ Blog/src/app/repositories/user_repository.py
@@ -1,6 +1,14 @@
 from fastcrud import FastCRUD
 from ..models.users import User
-from ..schemas.user import  UserCreateInternal, UserRead, UserUpdate, UserUpdateInternal, UserDelete
+from ..schemas.user import (
+    UserCreateInternal,
+    UserRead,
+    UserUpdate,
+    UserUpdateInternal,
+    UserDelete,
+)
 
-CRUDUser = FastCRUD[User, UserCreateInternal, UserRead, UserUpdate, UserUpdateInternal, UserDelete]
+CRUDUser = FastCRUD[
+    User, UserCreateInternal, UserRead, UserUpdate, UserUpdateInternal, UserDelete
+]
 crud_users = CRUDUser(User)
