30c00241b8f6e01355d777676be66fc16760a4df
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-21 13:38:00 +0545

    Formatting files to make code style consistent and cleaner

--- Blog/src/app/schemas/post.py
+++ Blog/src/app/schemas/post.py
@@ -8,23 +8,23 @@ from .comment import CommentRead
 class PostBase(BaseModel):
     title: Annotated[str, Field(min_length=2, max_length=100)]
     content: Annotated[str, Field(min_length=10, max_length=1000)]
-    
-    
-class Post(PostBase, TimestampSchema, PersistentDeletion, UUIDSchema): 
-    author_id: int 
+
+
+class Post(PostBase, TimestampSchema, PersistentDeletion, UUIDSchema):
+    author_id: int
 
     comments: Optional[List[CommentRead]] = None
 
 
 class PostRead(BaseModel):
-    id: int 
-    title: str 
-    content: str 
-    author_id: int 
+    id: int
+    title: str
+    content: str
+    author_id: int
     author_name: str
     number_of_comments: int
     view_count: int
-    created_at: datetime 
+    created_at: datetime
 
 
 class PostReadWithComments(PostRead):
@@ -38,14 +38,14 @@ class PostCreate(PostBase):
 class PostCreateInternal(PostBase):
     author_id: int
     author_name: str
-    
+
 
 class PostUpdate(BaseModel):
     model_config = ConfigDict(extra="forbid")
 
     title: Annotated[str, Field(min_length=2, max_length=200, default=None)]
-    content: Annotated[str, Field(min_length=10, max_length=1000, default=None)] 
-    
+    content: Annotated[str, Field(min_length=10, max_length=1000, default=None)]
+
 
 class PostUpdateInternal(PostUpdate):
     updated_at: datetime
@@ -56,6 +56,3 @@ class PostDelete(BaseModel):
 
     is_deleted: bool = True
     deleted_at: datetime
-
-
-
