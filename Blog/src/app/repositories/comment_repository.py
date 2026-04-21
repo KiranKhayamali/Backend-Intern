0915c2c39af63035f6e9dc089bbd2eb43de00b61
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-21 13:37:30 +0545

    Formatting files to make code style consistent and cleaner

--- Blog/src/app/repositories/comment_repository.py
+++ Blog/src/app/repositories/comment_repository.py
@@ -1,7 +1,19 @@
 from fastcrud import FastCRUD
-from ..models.comments import Comment 
-from ..schemas.comment import CommentCreateInternal, CommentRead, CommentUpdate, CommentUpdateInternal, CommentDelete
+from ..models.comments import Comment
+from ..schemas.comment import (
+    CommentCreateInternal,
+    CommentRead,
+    CommentUpdate,
+    CommentUpdateInternal,
+    CommentDelete,
+)
 
-
-CRUDComments = FastCRUD[Comment, CommentCreateInternal, CommentRead, CommentUpdate, CommentUpdateInternal, CommentDelete]
+CRUDComments = FastCRUD[
+    Comment,
+    CommentCreateInternal,
+    CommentRead,
+    CommentUpdate,
+    CommentUpdateInternal,
+    CommentDelete,
+]
 crud_comments = CRUDComments(Comment)
