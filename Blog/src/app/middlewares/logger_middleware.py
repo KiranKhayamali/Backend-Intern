dc5eee391f5766b9d2443c42794cf92e8f251a9a
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-21 13:36:37 +0545

    Formatting files to make code style consistent and cleaner

--- Blog/src/app/middlewares/logger_middleware.py
+++ Blog/src/app/middlewares/logger_middleware.py
@@ -1,19 +1,20 @@
-import uuid 
+import uuid
 import time
 import structlog
 from fastapi import FastAPI, Request
 from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
 from starlette.responses import Response
 
-
 logger = structlog.get_logger(__name__)
 
 
 class LoggerMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app:FastAPI) -> None:
+    def __init__(self, app: FastAPI) -> None:
         super().__init__(app)
 
-    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+    async def dispatch(
+        self, request: Request, call_next: RequestResponseEndpoint
+    ) -> Response:
         started_at = time.perf_counter()
         request_id = request.headers.get("X-Request-ID", str(uuid.uuid4()))
         structlog.contextvars.clear_contextvars()
@@ -21,7 +22,7 @@ class LoggerMiddleware(BaseHTTPMiddleware):
             request_id=request_id,
             client_host=request.client.host if request.client else None,
             path=request.url.path,
-            method=request.method
+            method=request.method,
         )
         response = await call_next(request)
         structlog.contextvars.bind_contextvars(status_code=response.status_code)
