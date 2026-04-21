cb7381ba9a0fc454209ade33dd060bc3617dc5f6
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-21 13:34:43 +0545

    Formatting files to make code style consistent and cleaner

--- Blog/src/app/api/logout.py
+++ Blog/src/app/api/logout.py
@@ -1,38 +1,25 @@
-from typing import Optional, Annotated, Any
-from fastapi import APIRouter, Cookie, Depends, Response
-from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Optional, Any
+from fastapi import APIRouter, Cookie, Response
 
 from ..core.exceptions.http_exceptions import UnauthorizedException
-from ..core.security import blacklist_tokens, oauth2_scheme
-from ..core.db.session import async_get_db
 from ..core.db.redis_connect import redis_client
-
 
 router = APIRouter(tags=["login"], prefix="/users/auth")
 
 
 @router.post("/logout")
 async def logout(
-    response: Response, 
-    access_token: str = Depends(oauth2_scheme), 
-    refresh_token: Optional[str] = Cookie(None, alias="refresh_token"), 
-    db: AsyncSession = Depends(async_get_db)
+    response: Response,
+    refresh_token: Optional[str] = Cookie(None, alias="refresh_token"),
 ) -> dict[str, Any]:
     try:
         if not refresh_token:
             raise UnauthorizedException("Refresh Token Not Found!!!")
-        
-        if access_token.startswith("Bearer "):
-            access_token = access_token.split(" ")[1]
-        
-        await blacklist_tokens(access_token=access_token, db=db)
 
         redis_client.delete(f"auth:refresh:{refresh_token}")
         response.delete_cookie(key="refresh_token")
 
         return {"message": "Logged Out Successfully!!!"}
-    
+
     except Exception as exec:
         raise UnauthorizedException(f"Invalid Token!!! Details: {exec}")
-
-
