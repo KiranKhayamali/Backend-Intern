59955bcc0513c6fec29a3437ce15ac72e38d8eea
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-21 12:00:11 +0545

    Restating logger feature

--- Blog/src/app/core/dependencies.py
+++ Blog/src/app/core/dependencies.py
@@ -8,12 +8,12 @@ from .db.session import async_get_db
 from ..repositories.user_repository import crud_users
 from ..schemas.user import UserRead
 from .security import TokenType, oauth2_scheme, verify_token
-# from .logger import logging
+from .logger import logging
 
 
 SessionDep = Annotated[AsyncSession, Depends(async_get_db)]
 
-# logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: SessionDep):
@@ -38,27 +38,27 @@ async def get_current_active_user(current_user: Annotated[UserRead, Depends(get_
     return current_user
 
 
-# async def get_optional_user(request: Request, db:SessionDep) -> dict | None:
-#     token = request.headers.get("Authorization")
-#     if not token:
-#         return None 
+async def get_optional_user(request: Request, db:SessionDep) -> dict | None:
+    token = request.headers.get("Authorization")
+    if not token:
+        return None 
 
-#     try:
-#         token_type, _, token_value = token.partition(" ")
-#         if token_type.lower() != "bearer" or not token_value:
-#             return None 
+    try:
+        token_type, _, token_value = token.partition(" ")
+        if token_type.lower() != "bearer" or not token_value:
+            return None 
 
-#         token_data = await verify_token(token_value, TokenType.ACCESS, db)
-#         if token_data is None:
-#             return None 
+        token_data = await verify_token(token_value, TokenType.ACCESS, db)
+        if token_data is None:
+            return None 
 
-#         return await get_current_user(token_value, db=db)
+        return await get_current_user(token_value, db=db)
     
-#     except HTTPException as http_exec:
-#         if http_exec.status_code != 401:
-#             logger.error(f"Unexpected HTTPExection in get_optional_user: {http_exec.detail}")
-#         return None 
+    except HTTPException as http_exec:
+        if http_exec.status_code != 401:
+            logger.error(f"Unexpected HTTPExection in get_optional_user: {http_exec.detail}")
+        return None 
 
-#     except Exception as exec:
-#         logger.error(f"Unexpected error in get_optional_user: {exec}")
-#         return None
+    except Exception as exec:
+        logger.error(f"Unexpected error in get_optional_user: {exec}")
+        return None
