306e45c84ac37467d6035d38b11125fdb31025f2
Kiran Khayamali <kirankhayamali@gmail.com>
2026-04-21 13:35:11 +0545

    Formatting files to make code style consistent and cleaner

--- Blog/src/app/core/db/database.py
+++ Blog/src/app/core/db/database.py
@@ -2,13 +2,13 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from ..config import settings
 from ...models.base import Base
 
-
 DATABASE_URI = settings.POSTGRES_URI
 DATABASE_PREFIX = settings.POSTGRES_ASYNC_PREFIX
 DATABASE_URL = f"{DATABASE_PREFIX}{DATABASE_URI}"
 
 
 async_engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+
 
 async def init_db() -> None:
     async with async_engine.begin() as conn:
