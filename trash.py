aec56dcae8b1be1e078cf001bce5ca35c4a9e09f
Kiran Khayamali <kirankhayamali@gmail.com>
2026-05-04 07:38:07 +0545

    Minor fixes

--- trash.py
+++ trash.py
@@ -4,3 +4,12 @@ import os
 name = os.getenv("MY_NAME", "Kiran Khayamli") # Default value is "Kiran Khayamli" if MY_NAME is not set
 
 print(f"Hello, I am {name} and I am accessing the name variable from the environment variable MY_NAME")
+
+
+
+
+
+
+
+
+
