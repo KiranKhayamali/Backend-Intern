e651f3cb2c5bf2152af5b01307d529ae65737e42
Kiran Khayamali <kirankhayamali@gmail.com>
2026-05-04 08:40:02 +0545

    Global dependency

---
+++ file_structured_todo/core/deps.py
@@ -0,0 +1,14 @@
+from typing_extensions import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends
+
+from .db.session import sessionLocal
+
+async def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        await db.close()
+
+SessionDep = Annotated[AsyncSession, Depends(get_db)]
