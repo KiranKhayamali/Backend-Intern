3a14065170a7ee01a90f28a8b703bb14263b03f9
Kiran Khayamali <kirankhayamali@gmail.com>
2026-05-04 08:46:40 +0545

    Added Execption

--- file_structured_todo/core/deps.py
+++ file_structured_todo/core/deps.py
@@ -1,14 +1,20 @@
 from typing_extensions import Annotated
+from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
 
 from .db.session import sessionLocal
 
-async def get_db():
-    db = sessionLocal()
-    try:
-        yield db
-    finally:
-        await db.close()
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with sessionLocal() as db:
+        try:
+            yield db
+            await db.commit()
+        except Exception as e:
+            await db.rollback()
+            raise
+        finally:
+            await db.close()
+
 
 SessionDep = Annotated[AsyncSession, Depends(get_db)]
